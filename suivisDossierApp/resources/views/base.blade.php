8eb78548409db09fef9b20a2929e51b3c68e573b
julescesare <djangbadjajules830@gmail.com>
2026-05-17 14:38:47 +0000

    feat:integration du template de base

---
+++ suivisDossierApp/resources/views/base.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard - Voler Admin Dashboard</title>
+
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/bootstrap.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/chartjs/Chart.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/perfect-scrollbar/perfect-scrollbar.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/app.css') }}">
+    <link rel="shortcut icon" href="{{ asset('admin/assets/images/favicon.svg') }}" type="image/x-icon">
+</head>
+
+<body>
+    <div id="app">
+        <!-- Sidebar -->
+        @include('partials.sidebar')
+        <div id="main">
+            <!-- Navbar -->
+            @include('partials.header')
+            <!-- Main Content -->
+            <div class="main-content container-fluid">
+                @yield('content')
+                <!-- Footer -->
+                @include('partials.footer')
+            </div>
+        </div>
+        <script src="{{ asset('admin/assets/js/feather-icons/feather.min.js') }}"></script>
+        <script src="{{ asset('admin/assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+        <script src="{{ asset('admin/assets/js/app.js') }}"></script>
+
+        <script src="{{ asset('admin/assets/vendors/chartjs/Chart.min.js') }}"></script>
+        <script src="{{ asset('admin/assets/vendors/apexcharts/apexcharts.min.js') }}"></script>
+        <script src="{{ asset('admin/assets/js/pages/dashboard.js') }}"></script>
+
+        <script src="{{ asset('admin/assets/js/main.js') }}"></script>
+</body>
+
+</html>
--- suivisDossierApp/routes/web.php
+++ suivisDossierApp/routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('base');
 });
+Route::get('/dashboard', function () {
+    return view('pages.dashboard');
+})->name('dashboard');
